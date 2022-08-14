6a2e6466beeb446eacdd4388a98d088001d01aa7
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-08-14 10:19:50 +0000

    added backgroundColor events

--- js/event.js
+++ js/event.js
@@ -1,1 +1,21 @@
-console.log("Plz enter your name:")
+console.log("Plz enter your name:");
+//type-2
+function makeGreen(){
+    document.body.style.backgroundColor='green';
+}
+
+// type-3
+const makeBuleButton= document.getElementById('make-blue');
+makeBuleButton.onclick=makeBule;
+
+function makeBule(){
+    document.body.style.backgroundColor='blue';
+}
+
+// type-3 another
+
+const makePurpleButton=document.getElementById('make-purple');
+       makePurpleButton.onclick = function makePurple(){
+        document.body.style.backgroundColor='purple';
+       }
+        
